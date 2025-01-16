43286bedef8ef7e4b79ff210b14edf08cce5666a
vishwang suthar <100853098+Vishwang0Suthar@users.noreply.github.com>
2025-01-16 14:50:12 +0530

    setting up reports page

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 
 const geminiApiKey = process.env.GEMINI_API_KEY;
+const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
 type Props = {};
 
 const Report = (props: Props) => {
