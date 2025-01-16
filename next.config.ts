9750655cc239ae6d62e5683ceaad1020d206c55a
vishwang suthar <100853098+Vishwang0Suthar@users.noreply.github.com>
2025-01-16 22:11:07 +0530

    AI image analysis on report page working

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
   env:{
     CONNECTION_URL: process.env.CONNECTION_URL,
     WEB3_AUTH_CLIENT_ID: process.env.WEB3_AUTH_CLIENT_ID,
-
+    GEMINI_API_KEY:process.env.GEMINI_API_KEY,
+    GOOGLE_MAPS_API_KEY:process.env.GOOGLE_MAPS_API_KEY
   }
 };
 
